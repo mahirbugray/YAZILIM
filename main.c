d96d68432e5ca99ef726fd013906ab246541a7d6
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2020-11-17 19:47:57 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,258 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int plaka_kodu;
+   printf("ilin plaka kodunu giriniz: ");
+   scanf("%d",&plaka_kodu);
+
+   switch (plaka_kodu){
+   case 1:
+      printf("ADANA\n");
+      break;
+   case 2:
+    printf("ADIYAMAN\n");
+      break;
+   case 3:
+    printf("AFYON\n");
+      break;
+   case 4:
+    printf("AÐRI\n");
+     break;
+   case 5:
+    printf("AMASYA\n");
+     break;
+   case 6:
+    printf("ANKARA\n");
+     break;
+   case 7:
+    printf("ANTALYA\n");
+     break;
+   case 8:
+    printf("ARTVIN\n");
+     break;
+   case 9:
+    printf("AYDIN\n");
+     break;
+   case 10:
+    printf("BALIKESIR\n");
+     break;
+   case 11:
+    printf("BILECIK\n");
+     break;
+   case 12:
+    printf("BINGOL\n");
+     break;
+   case 13:
+    printf("BITLIS\n");
+     break;
+   case 14:
+    printf("BOLU\n");
+     break;
+   case 15:
+    printf("BURDUR\n");
+     break;
+   case 16:
+    printf("BURSA\n");
+     break;
+   case 17:
+    printf("CANAKKALE\n");
+     break;
+   case 18:
+    printf("CANKIRI\n");
+     break;
+   case 19:
+    printf("CORUM\n");
+     break;
+   case 20:
+    printf("DENIZLI\n");
+     break;
+   case 21:
+    printf("DIYARBAKIR\n");
+     break;
+   case 22:
+    printf("EDIRNE\n");
+     break;
+   case 23:
+    printf("ELAZIG\n");
+     break;
+   case 24:
+    printf("ERZINCAN\n");
+     break;
+   case 25:
+    printf("ERZURUM\n");
+     break;
+   case 26:
+    printf("ESKISEHIR\n");
+     break;
+   case 27:
+    printf("GAZIANTEP\n");
+     break;
+   case 28:
+    printf("GIRESUN\n");
+     break;
+   case 29:
+    printf("GUMUSHANE\n");
+     break;
+   case 30:
+    printf("HAKKARI\n");
+     break;
+   case 31:
+    printf("HATAY\n");
+     break;
+   case 32:
+    printf("ISPARTA\n");
+     break;
+   case 33:
+    printf("MERSIN\n");
+     break;
+   case 34:
+    printf("ISTANBUL\n");
+     break;
+   case 35:
+    printf("IZMIR\n");
+     break;
+   case 36:
+    printf("KARS\n");
+     break;
+   case 37:
+    printf("KASTAMONU\n");
+     break;
+   case 38:
+    printf("KAYSERI\n");
+     break;
+   case 39:
+    printf("KIRKLARELI\n");
+     break;
+   case 40:
+    printf("KIRSEHIR\n");
+     break;
+   case 41:
+    printf("KOCAELI\n");
+     break;
+   case 42:
+    printf("KONYA\n");
+     break;
+   case 43:
+    printf("KUTAHYA\n");
+     break;
+   case 44:
+    printf("MALATYA\n");
+     break;
+   case 45:
+    printf("MANISA\n");
+     break;
+   case 46:
+    printf("KAHRAMANMARAS\n");
+     break;
+   case 47:
+    printf("MARDIN\n");
+     break;
+   case 48:
+    printf("MUGLA\n");
+     break;
+   case 49:
+    printf("MUS\n");
+     break;
+   case 50:
+    printf("NEVSEHIR\n");
+     break;
+   case 51:
+    printf("NIGDE\n");
+     break;
+   case 52:
+    printf("ORDU\n");
+     break;
+   case 53:
+    printf("RIZE\n");
+     break;
+   case 54:
+    printf("SAKARYA\n");
+     break;
+   case 55:
+    printf("SAMSUN\n");
+     break;
+   case 56:
+    printf("SIIRT\n");
+     break;
+   case 57:
+    printf("SINOP\n");
+     break;
+   case 58:
+    printf("SIVAS\n");
+     break;
+   case 59:
+    printf("TEKIRDAG\n");
+     break;
+   case 60:
+    printf("TOKAT\n");
+     break;
+   case 61:
+    printf("TRABZON\n");
+     break;
+   case 62:
+    printf("TUNCELI\n");
+     break;
+   case 63:
+    printf("SANLIURFA\n");
+     break;
+   case 64:
+    printf("USAK\n");
+     break;
+   case 65:
+    printf("VAN\n");
+     break;
+   case 66:
+    printf("YOZGAT\n");
+     break;
+   case 67:
+    printf("ZONGULDAK\n");
+     break;
+   case 68:
+    printf("AKSARAY\n");
+     break;
+   case 69:
+    printf("BAYBURT\n");
+     break;
+   case 70:
+    printf("KARAMAN\n");
+     break;
+   case 71:
+    printf("KIRIKKALE\n");
+     break;
+   case 72:
+    printf("BATMAN\n");
+     break;
+   case 73:
+    printf("SIRNAK\n");
+     break;
+   case 74:
+    printf("BARTIN\n");
+     break;
+   case 75:
+    printf("ARDAHAN\n");
+     break;
+   case 76:
+    printf("IGDIR\n");
+     break;
+   case 77:
+    printf("YALOVA\n");
+     break;
+   case 78:
+    printf("KARABUK\n");
+     break;
+   case 79:
+    printf("KILIS\n");
+     break;
+   case 80:
+    printf("OSMANIYE\n");
+     break;
+   case 81:
+    printf("DUZCE\n");
+     break;
+
+}
+    return 0;
+}
+
