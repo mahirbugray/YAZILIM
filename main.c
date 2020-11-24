832e6e9ca049e6073093a9f06b1c238e9b6ed3e0
mahirbugray <74426348+mahirbugray@users.noreply.github.com>
2020-11-24 22:32:09 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,20 +3,13 @@
 
 int main()
 {
-int sayac, baslangic, bitis;
-int toplam=0;
+    int girilen_sayi, toplam=0;
 
-printf("Baslangic sayisini giriniz:");
-scanf("%d" ,&baslangic);
-
-printf("bitis sayisini giriniz:");
-scanf("%d" ,&bitis);
-
-for (sayac=baslangic; sayac<=bitis; sayac++){
- toplam = sayac + toplam;
+     while (girilen_sayi != 0){
+            printf("Bir sayi giriniz:");
+            scanf("%d",&girilen_sayi);
+            toplam += girilen_sayi;
+     }
+         printf("toplam:%d", toplam);
+      return 0;
 }
- printf("toplam:%d",toplam);
-
- return 0;
-}
-
